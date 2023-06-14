c73948f4bd6cf7a3ccca9790d3330585c2107b50
Stephanie Cruz <stephaniecruzcastelli@gmail.com>
2023-06-14 10:04:07 -0400

    Se agrega frase

--- src/sections/Quote.jsx
+++ src/sections/Quote.jsx
@@ -1,8 +1,9 @@
 import { Typography } from '@material-tailwind/react'
 import quote from '../assets/quote.jpg'
+import { useTranslation } from 'react-i18next';
 
 export default function Quote() {
-
+    const { t } = useTranslation();
   
   return (
     <>
@@ -10,10 +11,10 @@ export default function Quote() {
            <div className="grid grid-cols-1 md:grid-cols-2">
                 <div className="flex flex-col items-end justify-center px-10 sm:px-20 md:px-10 lg:px-20 py-20">
                     <Typography variant="lead" className="text-end text-blue-gray-900 font-poppins italic">
-                        “Lorem ipsum dolor sit ammen conseteur, lorem ipsum sit ammen”
+                        “{t('quote')}”
                     </Typography>
                     <Typography variant="lead" className="text-end text-blue-gray-900 font-poppins italic mt-4 text-base">
-                        - Nombre Autor
+                        - Theodore Roosevelt
                     </Typography>
                 </div>
                 <div className="relativetive bg-center bg-cover bg-no-repeat h-[300px]"  style={{ backgroundImage: "url(" + quote + ")" }}>
