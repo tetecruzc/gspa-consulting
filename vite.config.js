5786efb7da3959551e0c503fb1e38650791b0455
Stephanie Cruz <stephaniecruzcastelli@gmail.com>
2023-06-13 23:01:40 -0400

    Se comprimen imagenes

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the size limit as needed
+    outDir: 'dist', // Output directory for the build
+    assetsDir: '', // Directory for static assets (JS, CSS, images, etc.)
+    sourcemap: true, // Generate source maps for debugging
+    // ... other configurations
+  },
 })
